1add9bfaac4f32de198850b6459ffd0b475962fe
murueka <298748+3c1u@users.noreply.github.com>
2018-11-18 21:27:43 +0900

    ADD clapによる引数パース

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,12 @@
 
 #[macro_use]
 extern crate nom;
+extern crate clap;
 extern crate libc;
 
-use std::env;
+use clap::{App, Arg, SubCommand};
+
+use std::io::{Error, ErrorKind};
 
 pub mod ast;
 pub mod eval;
@@ -23,12 +26,71 @@ mod cl {
     }
 }
 
-fn main() {
+fn parse_args() -> Result<(), std::io::Error> {
+    let matches = App::new("maysick")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(
+            SubCommand::with_name("run")
+                .version(env!("CARGO_PKG_VERSION"))
+                .author(env!("CARGO_PKG_AUTHORS"))
+                .about("Runs a maysick program with interpreter")
+                .arg(
+                    Arg::with_name("INPUT")
+                        .help("Specify a directory which includes \"run/\"")
+                        .required(true)
+                        .index(1),
+                ),
+        ).subcommand(
+            SubCommand::with_name("compile")
+                .version(env!("CARGO_PKG_VERSION"))
+                .author(env!("CARGO_PKG_AUTHORS"))
+                .about("Compiles a maysick program into native code")
+                .arg(
+                    Arg::with_name("INPUT")
+                        .help("Specify a directory which includes \"run/\"")
+                        .required(true)
+                        .index(1),
+                ).arg(
+                    Arg::with_name("bin_output")
+                        .short("o")
+                        .long("output")
+                        .value_name("FILE")
+                        .help("Writes output to the file")
+                        .takes_value(true),
+                ).arg(
+                    Arg::with_name("emitllvm")
+                        .short("e")
+                        .long("emit-llvm")
+                        .help("Emits an LLVM code"),
+                ),
+        ).get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("run") {
+        let cpath = matches.value_of("INPUT").ok_or(Error::new(
+            ErrorKind::InvalidInput,
+            "Input file is not specified.",
+        ))?;
+        loader::run(cpath);
+    } else if let Some(matches) = matches.subcommand_matches("compile") {
+        let _cpath = matches.value_of("INPUT").ok_or(Error::new(
+            ErrorKind::InvalidInput,
+            "Input file is not specified.",
+        ))?;
+        let _bpath = matches.value_of("INPUT").unwrap_or("a.out");
+        let _lflag = matches.occurrences_of("e") > 0;
+        // Yah build!!
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), std::io::Error> {
+    // libcの乱数生成器の初期化
     unsafe {
         let t = cl::clock();
         libc::srand(t as u32);
     }
-
-    let cpath = env::args().nth(1).unwrap_or(".".to_string());
-    loader::run(&cpath.as_str());
+    parse_args()
 }
